d021a30631c9850197368a98318a88e43a59e4aa
Hurtubise-David <hurtubisedavid@gmail.com>
2025-07-24 16:02:10 -0400

    Add draw_matches and inliner, update_params

--- MiniSLAM/feature_tracker.py
+++ MiniSLAM/feature_tracker.py
@@ -2,61 +2,63 @@ import cv2
 import numpy as np
 
 class ORBTracker:
-    def __init__(self, max_features=2000):
-        self.orb = cv2.ORB_create(nfeatures=max_features)
+    def __init__(self, max_features=2000, ratio=0.8, checks=50):
+        self.max_features = max_features
+        self.ratio = ratio
+        self.checks = checks
 
-        # FLANN pour ORB (avec LSH = Locality-Sensitive Hashing)
-        index_params = dict(algorithm=6,  # FLANN_INDEX_LSH
-                            table_number=6,
-                            key_size=12,
-                            multi_probe_level=1)
-        search_params = dict(checks=50)
-        self.matcher = cv2.FlannBasedMatcher(index_params, search_params)
+        self.orb = cv2.ORB_create(nfeatures=self.max_features)
+
+        self.index_params = dict(algorithm=6, table_number=6, key_size=12, multi_probe_level=1)
+        self.search_params = dict(checks=self.checks)
+        self.matcher = cv2.FlannBasedMatcher(self.index_params, self.search_params)
 
     def detect(self, image):
-        """
-        Détecte les keypoints et calcule les descripteurs ORB
-        :param image: image (gauche) actuelle
-        :return: keypoints, descripteurs
-        """
         keypoints, descriptors = self.orb.detectAndCompute(image, None)
-        return keypoints, descriptors            
+        return keypoints, descriptors
 
-    def match(self, kp1, des1, kp2, des2, ratio=0.8):
-        """
-        Matche les descripteurs entre deux frames avec le ratio test
-        :param kp1: keypoints image t-1
-        :param des1: descripteurs t-1
-        :param kp2: keypoints image t
-        :param des2: descripteurs t
-        :return: matches filtrés, pts1, pts2
-        """
+    def match(self, kp1, des1, kp2, des2):
         if des1 is None or des2 is None or len(des1) < 2 or len(des2) < 2:
-            return [], np.array([]), np.array([])  # aucun match possible
-    
-        matches = self.matcher.knnMatch(des1, des2, k=2)
+            return [], np.array([]), np.array([])
 
+        matches = self.matcher.knnMatch(des1, des2, k=2)
         good_matches = []
         pts1 = []
         pts2 = []
 
         for m_n in matches:
             if len(m_n) != 2:
-                continue  # on saute les cas où il n'y a pas 2 matches
-
-            m, n = m_n 
-
-            if m.distance < ratio * n.distance:
+                continue
+            m, n = m_n
+            if m.distance < self.ratio * n.distance:
                 good_matches.append(m)
                 pts1.append(kp1[m.queryIdx].pt)
                 pts2.append(kp2[m.trainIdx].pt)
 
         return good_matches, np.array(pts1), np.array(pts2)
-    
-    def filter_with_ransac(pts1, pts2, K):
+
+    def filter_with_ransac(self, pts1, pts2, K):
         E, mask = cv2.findEssentialMat(pts1, pts2, K, method=cv2.RANSAC, prob=0.999, threshold=1.0)
         if E is None or mask is None:
             return None, None, None
         inliers1 = pts1[mask.ravel() == 1]
         inliers2 = pts2[mask.ravel() == 1]
         return inliers1, inliers2, mask
+
+    def draw_matches(self, img1, kp1, img2, kp2, matches):
+        return cv2.drawMatches(img1, kp1, img2, kp2, matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    def draw_inlier_matches(self, img1, kp1, img2, kp2, matches, mask):
+        inlier_matches = [m for i, m in enumerate(matches) if mask[i]]
+        return cv2.drawMatches(img1, kp1, img2, kp2, inlier_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    def update_params(self, max_features=None, ratio=None, checks=None):
+        if max_features is not None:
+            self.max_features = max_features
+            self.orb = cv2.ORB_create(nfeatures=self.max_features)
+        if ratio is not None:
+            self.ratio = ratio
+        if checks is not None:
+            self.checks = checks
+            self.search_params = dict(checks=self.checks)
+            self.matcher = cv2.FlannBasedMatcher(self.index_params, self.search_params)
