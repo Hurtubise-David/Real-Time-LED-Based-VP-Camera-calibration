7830b7dfa2538bbfb00bc98f0d39e02a74cf7470
Hurtubise-David <hurtubisedavid@gmail.com>
2025-07-24 12:11:44 -0400

    Class ORBTracker, init feature detector

---
+++ MiniSLAM/feature_tracker.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+Class ORBTracker:
+    def __init__(self, max_features=2000):
+        self.orb = cv2.ORB_create(nfeatures=max_features)
+
+        # FLANN pour ORB (avec LSH = Locality-Sensitive Hashing)
+        index_params = dict(algorithm=6,  # FLANN_INDEX_LSH
+                            table_number=6,
+                            key_size=12,
+                            multi_probe_level=1)
+        search_params = dict(checks=50)
+        self.matcher = cv2.FlannBasedMatcher(index_params, search_params)
+        
+            
