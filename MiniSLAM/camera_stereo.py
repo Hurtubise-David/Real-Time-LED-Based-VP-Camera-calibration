1704c913b57484f731fb488c88f1842b33b7fd95
Hurtubise-David <hurtubisedavid@gmail.com>
2025-07-24 11:52:42 -0400

    Initialise Camera ELP and intrinsic preset parameters

--- MiniSLAM/camera_stereo.py
+++ MiniSLAM/camera_stereo.py
@@ -4,3 +4,16 @@ import numpy as np
 
 class StereoCamera:
     def __init__(self, camera_id=0, width=1280, height=480, fps=60)
+        """
+        Initialise une caméra stéréo avec flux côte à côte (ELP).
+        :param camera_id: index de la caméra combinée
+        """
+        self.cap = cv2.VideoCapture(camera_id)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        self.cap.set(cv2.CAP_PROP_FPS, fps)
+
+        # Paramètres de calibration fictifs (remplacer par réels plus tard)
+        self.K = np.array([[640, 0, 320],
+                           [0, 640, 240],
+                           [0,   0,   1]], dtype=np.float32)
