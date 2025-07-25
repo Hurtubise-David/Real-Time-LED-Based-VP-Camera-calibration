e0c965cbcd23fba3684a5b3b976ee315e07db88b
Hurtubise-David <hurtubisedavid@gmail.com>
2025-07-25 17:23:14 -0400

    define node in graph

--- SPHEIRE-SLAM/pose_graph.py
+++ SPHEIRE-SLAM/pose_graph.py
@@ -9,10 +9,14 @@ class PoseEdge:
     def __init__(self, from_id, to_id, relative_pose, information=np.eye(6)):
         self.from_id = from_id
         self.to_id = to_id
-        self.relative_pose = relative_pose  # 4x4 relative transformation
-        self.information = information      # 6x6 information matrix (optional for now)
+        self.relative_pose = relative_pose  # 4x4 transformation relative
+        self.information = information      # 6x6 information matrix (optionel)
 
 class PoseGraph:
     def __init__(self):
         self.nodes = {}  # key: frame_id, value: PoseNode
-        self.edges = []  # list of PoseEdge
+        self.edges = []  # liste de PoseEdge
+
+    def add_node(self, frame_id, pose):
+        if frame_id not in self.nodes:
+            self.nodes[frame_id] = PoseNode(frame_id, pose)
