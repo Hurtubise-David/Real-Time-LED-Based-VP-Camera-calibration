422bb4fd9d1fbf0b676f2b558649c86cd0797f82
Hurtubise-David <hurtubisedavid@gmail.com>
2025-07-25 17:21:45 -0400

    create class poseEdge to define the edges of the graph

--- SPHEIRE-SLAM/pose_graph.py
+++ SPHEIRE-SLAM/pose_graph.py
@@ -4,3 +4,10 @@ class PoseNode:
     def __init__(self, frame_id, pose):
         self.frame_id = frame_id
         self.pose = pose  # 4x4 numpy array
+
+class PoseEdge:
+    def __init__(self, from_id, to_id, relative_pose, information=np.eye(6)):
+        self.from_id = from_id
+        self.to_id = to_id
+        self.relative_pose = relative_pose  # 4x4 relative transformation
+        self.information = information      # 6x6 information matrix (optional for now)
